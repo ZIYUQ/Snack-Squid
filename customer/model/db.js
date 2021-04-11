d63e7f6c87487ced049cfdac91c06c0c243f69e0
Cathy-kk <yu572398517@gmail.com>
2021-04-11 22:35:27 +0800

    connect ot database

---
+++ customer/model/db.js
@@ -0,0 +1,18 @@
+
+const mongoose = require("mongoose")
+
+// const order = new mongoose.Schema({
+//     id: {type: String, require=true, unique = true},
+//     fulfilled: {type:Boolean, default: false},
+// })
+
+// const menuSchema = new mongoose.Schema({
+//     name: {type:String, require:true},
+//     price:{type: Number, require:true},
+//     phote:String,
+//     type: String,
+//     description: String
+// })
+
+// const snack = mongoose.model("snack", menuSchema)
+// module.exports = {snack}
