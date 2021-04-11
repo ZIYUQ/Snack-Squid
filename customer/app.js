201020ec19e6fe4e8c8f076f23f7f4e9ed24ee8a
ztom <23621760+tzhern@users.noreply.github.com>
2021-04-11 23:13:54 +0800

    update

--- customer/app.js
+++ customer/app.js
@@ -4,10 +4,9 @@ const path = require('path')
 app.use(express.json())
 
 
-app.use(express.static('views/homepage'))
+app.use(express.static('public'))
 
-// 
-const {snack} = require("./model/index")
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/views/homepage.html'))
@@ -21,7 +20,7 @@ app.all('*', (req, res) => {  // 'default' route to catch user errors
     res.status(404).send('<p>invalid request</p>')
 })
 
-//
+
 app.get('/menu',  async (req, res) => {
     try{
         result = await snack.find({}, {name: true, price: true, _id:false})
@@ -30,7 +29,7 @@ app.get('/menu',  async (req, res) => {
         res.status(400)
         res.send("error")
     }
-   
+
 })
 
 const port = process.env.PORT || 3000
