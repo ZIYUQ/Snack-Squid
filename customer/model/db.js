ab07e0e938af7a2799f7634eba3c86e754b6e019
ztom <23621760+tzhern@users.noreply.github.com>
2021-04-13 00:00:52 +0800

    Update db.js

--- customer/model/db.js
+++ customer/model/db.js
@@ -2,6 +2,7 @@
 const mongoose = require("mongoose")
 
 
+<<<<<<< HEAD
 // menu model
 const menuSchema = new mongoose.Schema({
     name: {type:String, require:true},
@@ -13,3 +14,15 @@ const menuSchema = new mongoose.Schema({
 
 const menu = mongoose.model("menu", menuSchema)
 module.exports = {menu}
+=======
+// const menuSchema = new mongoose.Schema({
+//     name: {type:String, require:true},
+//     price:{type: Number, require:true},
+//     phote:String,
+//     type: String,
+//     description: String
+// })
+
+// const snack = mongoose.model("snack", menuSchema)
+// module.exports = {snack}
+>>>>>>> parent of 5c10c9d (make it better)
