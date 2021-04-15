85150387db29a3a16bda489c361281870bf57fc7
Ziyuq <ziyuq@student.unimelb.edu.au>
2021-04-15 23:58:57 +1000

    Update app.js

--- vendor/app.js
+++ vendor/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const bodyParser = require('body-parser')
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 
 app.use(express.json())
 app.use('/', express.static('html'))
