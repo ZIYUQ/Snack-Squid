2dd27ea55de4ce6329caff27ccfaa7d66784710b
ztom <23621760+tzhern@users.noreply.github.com>
2021-04-17 18:23:54 +0800

    Add expressValidator

--- customer/controllers/loginController.js
+++ customer/controllers/loginController.js
@@ -1,5 +1,8 @@
 const { Customer } = require('../model/customer')
 
+// express-validator, to validate user data in forms
+const expressValidator = require('express-validator')
+
 const login = async(req, res) => {
     let username = req.body.email_address
     let password = req.body.password
