7c08ce5dfa8c4ea25a359be7b9bc4e94318ba4fe
ztom <23621760+tzhern@users.noreply.github.com>
2021-04-22 14:05:32 +0800

    Update van.js
    change password-unique to false

--- snack-squid/model/van.js
+++ snack-squid/model/van.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 // Van model
 const vanSchema = new mongoose.Schema({
     vanName: { type: String, required: true, unique: true },
-    password: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
     emailAddress: { type: String, required: true },
     mobileNumber: { type: String, required: true },
     location: String,
