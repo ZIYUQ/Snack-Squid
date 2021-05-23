0a43ec5c7fffc1145f1027712768e0f6d3526257
ztom <23621760+tzhern@users.noreply.github.com>
2021-05-23 16:23:24 +0800

    Update orderController.js

--- snack-squid/controllers/customer/orderController.js
+++ snack-squid/controllers/customer/orderController.js
@@ -6,10 +6,9 @@ const Van = require("../../model/van")
 
 // params: [{food_id}, {quantity}]
 const placeOrder = async(req, res) => {
-
-    let cart = req.body
-        // get food price and calculate the total price
-    let totalPrice = 0
+    let cart = req.body;
+    // get food price and calculate the total price
+    let totalPrice = 0;
     for (let i = 0; i < cart.length; i++) {
         // get the food price
         let foodName = cart[i]["foodName"]
@@ -103,16 +102,16 @@ const getOrder = async(req, res) => {
 }
 
 const cancelOrder = async(req, res) => {
-    let id = req.params.orderId
-    if (id === undefined || id === null) {
+    let orderId = req.params.orderId
+    if (orderId === undefined || orderId === null) {
         return res.send("no order found")
     }
     try {
-        let result = await Order.findOne({ _id: id }, {})
+        let result = await Order.findOne({ _id: orderId }, {})
 
         // Set status as fulfilled
-        await Order.updateOne({ _id: id }, { $set: { status: 'cancelled' } })
-        console.log("cancel order successfully!")
+        await Order.updateOne({ _id: orderId }, { $set: { status: 'cancelled' } })
+        console.log("cancel order", orderId, "successfully!")
         return res.redirect('/customer/order')
 
     } catch (err) {
@@ -147,8 +146,6 @@ const changeOrder = async(req, res) => {
         }
 
         const orderChanged = { details: cart, total: totalPrice }
-
-        console.log(orderChanged)
 
         await Order.findOneAndUpdate({ _id: orderId }, orderChanged, { new: true });
         console.log("order", orderId, "updated successfully!")
