7ca8572d6c8e7ecac0db2823acb150d34d3320db
ztom <23621760+tzhern@users.noreply.github.com>
2021-05-27 23:46:05 +0800

    Fix flash bug

--- snack-squid/config/passport.js
+++ snack-squid/config/passport.js
@@ -57,7 +57,7 @@ module.exports = function(passport) {
                     else {
                         req.session.userId = user._id
                         console.log('Customer logged in successfully: ', req.session.userId)
-                        return done(null, user, req.flash('loginMessage', 'Login successful'));
+                        return done(null, user);
                     }
                 });
             });
@@ -140,7 +140,7 @@ module.exports = function(passport) {
                     else {
                         req.session.vanId = user._id;
                         console.log("Vendor logged in successfully:", req.session.vanId)
-                        return done(null, user, req.flash('loginMessage', 'Login successful'));
+                        return done(null, user);
                     }
                 });
             });
